acf5a3389f89b05ee7f20050f4ca71386a97ceae
AliAmet <ametov1ali@gmail.com>
2023-11-11 13:16:25 +0600

    first commit

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
-    }
+//        for (int i = 1; i <= height + (height -1); i++){
+//            for (int y = 1; y <= height; y++){
+//                if (i == 1 || i == height + (height - 1) || ((i == 2 || i == )))
+            }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int x =0; x < cathetusLength; x++){
+            for (int y = 0; y < cathetusLength; y++){
+                if (x >= y)
+                    System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }

774f6b76251b8f58877a222ef95193213adac64e
AliAmet <ametov1ali@gmail.com>
2023-11-11 13:23:43 +0600

    first commit

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,14 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-        for (int i = 0; i < sideLength; i++){
-            for (int y = 0; y < sideLength; y++){
-                if ((i == 0 || i == sideLength - 1) || (y == 0 || y == sideLength-1))
+        for (int i = 1; i <= sideLength; i++){
+            for (int y = 1; y <= sideLength; y++){
+//                if ((i == 0 || i == sideLength - 1) || (y == 0 || y == sideLength-1))
+//                    System.out.print("8");
+//                else
+//                    System.out.print(" ");
+                if (i == sideLength / 2  || y == sideLength / 2)
                     System.out.print("8");
                 else
                     System.out.print(" ");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Square square = new Square();
+        square.printSquareFrom8s(4);
     }
 }
